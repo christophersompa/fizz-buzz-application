3088f4999cabe49b27f894bff36cd3ec03101aab
Christopher <cgtsompa@gmail.com>
2024-10-09 15:17:51 +0100

    try & catch exception implemented

--- fizz-buzz-application/Program.cs
+++ fizz-buzz-application/Program.cs
@@ -12,34 +12,41 @@ namespace fizz_buzz_application
         {
             Console.WriteLine("Please enter a number:");
 
-            int userInput = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("\nProcessing...\n");
-
-            int number = 1;
-
-            while (number <= userInput)
+            try
             {
-                if (number % 3 == 0 && number % 5 != 0)
-                {
-                    Console.WriteLine("Fizz");
-                }
-                else if (number % 5 == 0 && number % 3 != 0)
-                {
-                    Console.WriteLine("Buzz");
-                }
-                else if (number % 3 == 0 && number % 5 == 0)
-                {
-                    Console.WriteLine("FizzBuzz");
-                }
-                else
-                {
-                    Console.WriteLine(number);
-                }
-                number++;
-            }
+                int userInput = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("\nDone.");
+                Console.WriteLine("\nProcessing...\n");
+
+                int number = 1;
+
+                while (number <= userInput)
+                {
+                    if (number % 3 == 0 && number % 5 != 0)
+                    {
+                        Console.WriteLine("Fizz");
+                    }
+                    else if (number % 5 == 0 && number % 3 != 0)
+                    {
+                        Console.WriteLine("Buzz");
+                    }
+                    else if (number % 3 == 0 && number % 5 == 0)
+                    {
+                        Console.WriteLine("FizzBuzz");
+                    }
+                    else
+                    {
+                        Console.WriteLine(number);
+                    }
+                    number++;
+                }
+
+                Console.WriteLine("\nDone.");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
 
             Console.ReadLine();
         }
