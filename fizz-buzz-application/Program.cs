289b0fb2c32f3c769cb314e3a6fe70e40ed0c873
Christopher <cgtsompa@gmail.com>
2024-10-09 15:09:30 +0100

    Add project files.

---
+++ fizz-buzz-application/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fizz_buzz_application
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number:");
+
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nProcessing...\n");
+
+            int number = 1;
+
+            while (number <= userInput)
+            {
+                if (number % 3 == 0 && number % 5 != 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (number % 5 == 0 && number % 3 != 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else if (number % 3 == 0 && number % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else
+                {
+                    Console.WriteLine(number);
+                }
+                number++;
+            }
+
+            Console.WriteLine("\nDone.");
+
+            Console.ReadLine();
+        }
+    }
+}
